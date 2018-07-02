0f8cff426ef63f54f9afda068d4b29de5dde9bea
= <tbennett3434@gmail.com>
2018-07-02 15:01:07 -0700

    swipe functio with error: Uncaught TypeError: Cannot read property 'addEventListener' of null at app.js:375

--- scripts/app.js
+++ scripts/app.js
@@ -90,6 +90,7 @@
     if (!card) {
       card = app.cardTemplate.cloneNode(true);
       card.classList.remove('cardTemplate');
+      card.setAttribute('id', 'gestureZone');
       card.querySelector('.location').textContent = data.label;
       card.removeAttribute('hidden');
       app.container.appendChild(card);
@@ -363,7 +364,46 @@
 })();
 
 
+//Swiping
+let touchstartX = 0;
+let touchstartY = 0;
+let touchendX = 0;
+let touchendY = 0;
 
+const gestureZone = document.getElementById('gestureZone');
+
+gestureZone.addEventListener('touchstart', function(event) {
+    touchstartX = event.changedTouches[0].screenX;
+    touchstartY = event.changedTouches[0].screenY;
+}, false);
+
+gestureZone.addEventListener('touchend', function(event) {
+    touchendX = event.changedTouches[0].screenX;
+    touchendY = event.changedTouches[0].screenY;
+    handleGesture();
+}, false);
+
+function handleGesture() {
+    if (touchendX < touchstartX) {
+        console.log('Swiped left');
+    }
+
+    if (touchendX > touchstartX) {
+        console.log('Swiped right');
+    }
+
+    if (touchendY < touchstartY) {
+        console.log('Swiped up');
+    }
+
+    if (touchendY > touchstartY) {
+       console.log('Swiped down');
+    }
+
+    if (touchendY === touchstartY) {
+       console.log('Tap');
+    }
+}
 
 
 
